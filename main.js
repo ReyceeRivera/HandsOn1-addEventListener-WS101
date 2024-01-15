ba51d1a5e9b783cea6e908aabc2e7c9cbe06508f
ReyceeRivera <149312572+ReyceeRivera@users.noreply.github.com>
2024-01-15 22:48:59 +0800

    main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+document.getElementById("Button 1").addEventListener("click", Button1);
+
+function Button1() {
+  alert("Button 1 was clicked!");
+}
+
+document.getElementById("Button 2").addEventListener("click", Button2);
+
+function Button2() {
+  alert("Button 2 was clicked!");
+}
+
+document.getElementById("Button 3").addEventListener("click", Button3);
+
+function Button3() {
+  alert("Button 3 was clicked!");
+}
+       
